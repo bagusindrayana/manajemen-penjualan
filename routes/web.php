a151507611f1198e48e06db1fb4013b7cc35a335
bagusindrayana <bagusindrayanaindo@gmail.com>
2025-01-11 22:03:04 +0800

    route to call artisan for filament optimize

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/filament-optimize', function () {
+    return Artisan::call('make:optimize');
+});
+
+Route::get('/filament-optimize-clear', function () {
+    return Artisan::call('make:optimize-clear');
+});
+
 
 Route::get('/', function () {
     return redirect('/admin');
