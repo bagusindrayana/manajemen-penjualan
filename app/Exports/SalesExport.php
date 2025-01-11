d5fc03ee5ef46daf95940bc8ac957253b17bd547
bagusindrayana <bagusindrayanaindo@gmail.com>
2025-01-11 17:03:46 +0800

    fix: total jumlah penjualan pada laporan penjualan

--- app/Exports/SalesExport.php
+++ app/Exports/SalesExport.php
@@ -7,11 +7,16 @@ use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithCalculatedFormulas;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
 
-class SalesExport implements FromCollection, WithHeadings, WithMapping, ShouldAutoSize
+class SalesExport implements FromCollection, WithHeadings, WithMapping, ShouldAutoSize, WithEvents, WithCalculatedFormulas
 {
     protected $startDate;
     protected $endDate;
+
+    private $total = 0;
 
     public function __construct($startDate, $endDate)
     {
@@ -45,7 +50,9 @@ class SalesExport implements FromCollection, WithHeadings, WithMapping, ShouldAu
     public function map($sale): array
     {
         $rows = [];
+        $total = 0;
         foreach ($sale->items as $item) {
+            $total += $item->subtotal;
             $rows[] = [
                 $sale->date->format('d/m/Y H:i'),
                 $sale->invoice_number,
@@ -53,10 +60,24 @@ class SalesExport implements FromCollection, WithHeadings, WithMapping, ShouldAu
                 $item->quantity,
                 $item->price,
                 $item->subtotal,
-                $sale->total_amount,
-                $sale->payment_method,
+                $total,
             ];
+            $this->total += $item->subtotal;
         }
         return $rows;
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function(AfterSheet $event) {
+                // $cellRange = 'F2:F' . ($event->sheet->getHighestRow() - 1);
+                // $event->sheet->setCellValue('F' . $event->sheet->getHighestRow()+1, '=SUM(' . $cellRange . ')');
+                // $cellRange = 'G2:G' . ($event->sheet->getHighestRow() - 1);
+                // $event->sheet->setCellValue('G' . $event->sheet->getHighestRow(), '=SUM(' . $cellRange . ')');
+                $event->sheet->setCellValue('A' . $event->sheet->getHighestRow()+1, "Total");
+                $event->sheet->setCellValue('G' . $event->sheet->getHighestRow(), $this->total);
+            },
+        ];
     }
 }
