5c4201a9330a1b09c54d7eda5b0fd9e93f58739b
bagusindrayana <bagusindrayanaindo@gmail.com>
2025-01-12 12:56:58 +0800

    provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Filament\Facades\Filament;
 use Filament\Navigation\NavigationItem;
+use Filament\Support\Colors\Color;
+use Filament\Support\Facades\FilamentColor;
 
 class AppServiceProvider extends ServiceProvider
 {   
@@ -36,5 +38,30 @@ class AppServiceProvider extends ServiceProvider
         //             ->sort(3),
         //     ]);
         // });
+
+        // FilamentColor::register([
+        //     'danger' => Color::Red,
+        //     'gray' => Color::Zinc,
+        //     'info' => Color::Blue,
+        //     'primary' => Color::Amber,
+        //     'success' => Color::Green,
+        //     'warning' => Color::Amber,
+        // ]);
+
+        // FilamentColor::register([
+        //     'danger' => [
+        //         50 => '254, 242, 242',
+        //         100 => '254, 226, 226',
+        //         200 => '254, 202, 202',
+        //         300 => '252, 165, 165',
+        //         400 => '248, 113, 113',
+        //         500 => '239, 68, 68',
+        //         600 => '220, 38, 38',
+        //         700 => '185, 28, 28',
+        //         800 => '153, 27, 27',
+        //         900 => '127, 29, 29',
+        //         950 => '69, 10, 10',
+        //     ],
+        // ]);
     }
 }
